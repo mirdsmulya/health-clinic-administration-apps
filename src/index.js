eda7e242127aec806c2be6bc0bcd917a2e386ed1
mirdsm <by_ms19@yahoo.com>
2019-03-30 22:24:00 +0700

    added stats in configureStore

--- src/index.js
+++ src/index.js
@@ -10,14 +10,14 @@ import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {loadDatas} from './actions/dataAction';
 import {loadAntrian} from './actions/antrianAction';
+import {loadStats} from './actions/statsAction';
 import '../node_modules/toastr/build/toastr.min.css';
 
 const store = configureStore();
 store.dispatch(loadDatas());
 store.dispatch(loadAntrian());
+store.dispatch(loadStats());
 
-//<Provider store={store}>
-//
 
 
 render(
